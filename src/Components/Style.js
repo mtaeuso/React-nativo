bbccf5a632e2378648374c9db80a842c03fea20d
mtaeuso <mtaeuso@gmail.com>
2021-02-20 00:00:23 -0300

    react nativo

---
+++ src/Components/Style.js
@@ -0,0 +1,43 @@
+
+const Style = {
+    body: {
+        backgroundColor: '#ADD8E6',
+        color: "#DAA520",
+        fontSize: 60,
+        height: "100%"
+    },
+    bodyCard: {
+        backgroundColor: '#ADD8E6',
+        color: "#483D8B",
+        fontSize: 60,
+        height: "105"
+    },
+    position: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    space: {
+        paddingTop: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: '700'
+    },
+    cardProducts: {
+        borderWidth: 3,
+        borderColor: "#8FBC8F",
+        borderRadius: 8,
+        padding: 15,
+        margin: 11,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 22,
+        paddingTop: 155
+    }
+}
+
+export default Style
